480289733078e865984478a83517e11a645dbb56
CPrimbee <cprimbee@gmail.com>
2021-04-09 18:02:59 -0300

    Created index and create posts

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,6 @@
+<h1>Cadastrar Novo Post</h1>
+<form action="{{ route('posts.store') }}" method="post">
+    <input type="text" name="title" id="title" placeholder="Título">
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea>
+    <button type="submit">Enviar</button>
+</form>
