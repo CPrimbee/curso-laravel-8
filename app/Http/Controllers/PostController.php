72deac714c419482d7bd911a2830e26e71c7af27
CPrimbee <cprimbee@gmail.com>
2021-04-12 17:59:26 -0300

    Post store and show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdatePost;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -19,5 +20,24 @@ class PostController extends Controller
     public function create()
     {
         return view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request)
+    {
+        $post = Post::create($request->all());
+
+        return redirect()->route('posts.index');
+    }
+
+    public function show($id)
+    {
+        //$post = Post::where('id', $id)->first();
+        $post = Post::find($id);
+
+        if (!$post) {
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
     }
 }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,6 +1,15 @@
 <h1>Cadastrar Novo Post</h1>
+
+@if ($errors->any())
+    <div>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </div>
+@endif
 <form action="{{ route('posts.store') }}" method="post">
-    <input type="text" name="title" id="title" placeholder="Título">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea>
+    @csrf
+    <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{ old('content') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
