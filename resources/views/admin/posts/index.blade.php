ffb896f1363810a3c634da572a3cee34cb5959b8
CPrimbee <cprimbee@gmail.com>
2021-04-14 13:23:31 -0300

    Aplicado template em outras páginas

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,36 +4,36 @@
 
 @section('content')
 
-<a href="{{ route('posts.create') }}">Criar Novo Post</a>
-<hr>
-@if (session('message'))
-    <div>
-        {{ session('message') }}
-    </div>
-@endif
+    <a href="{{ route('posts.create') }}">Criar Novo Post</a>
+    <hr>
+    @if (session('message'))
+        <div>
+            {{ session('message') }}
+        </div>
+    @endif
 
-<form action="{{ route('posts.search') }}" method="post">
-    @csrf
-    <input type="text" name="search" placeholder="Filtrar:">
-    <button type="submit">Filtrar</button>
-</form>
+    <form action="{{ route('posts.search') }}" method="post">
+        @csrf
+        <input type="text" name="search" placeholder="Filtrar:">
+        <button type="submit">Filtrar</button>
+    </form>
 
-<h1>Posts</h1>
+    <h1>Posts</h1>
 
-@foreach ($posts as $post)
-    <p>{{ $post->title }}
-        [
-            <a href="{{ route('posts.show', $post->id) }}">Ver</a> |
-            <a href="{{ route('posts.edit', $post->id) }}">Editar</a>
-        ]</p>
-@endforeach
+    @foreach ($posts as $post)
+        <p>{{ $post->title }}
+            [
+                <a href="{{ route('posts.show', $post->id) }}">Ver</a> |
+                <a href="{{ route('posts.edit', $post->id) }}">Editar</a>
+            ]</p>
+    @endforeach
 
-<hr>
+    <hr>
 
-@if (isset($filters))
-    {{ $posts->appends($filters)->links() }}
-@else
-    {{ $posts->links() }}
-@endif
+    @if (isset($filters))
+        {{ $posts->appends($filters)->links() }}
+    @else
+        {{ $posts->links() }}
+    @endif
 
 @endsection
