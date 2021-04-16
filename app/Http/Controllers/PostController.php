c6e5ef6cb6f064c4330283f56b3957d3f771372b
CPrimbee <cprimbee@gmail.com>
2021-04-16 13:29:11 -0300

    Validações de Upload

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUpdatePost;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
@@ -60,6 +61,9 @@ class PostController extends Controller
         if (!$post = Post::find($id))
             return redirect()->route('posts.index');
 
+        if (Storage::exists($post->image))
+            Storage::delete($post->image);
+
         $post->delete();
 
         return redirect()
@@ -82,7 +86,19 @@ class PostController extends Controller
             return redirect()->route('posts.index');
         }
 
-        $post->update($request->all());
+        $data = $request->all();
+
+        if ($request->image && $request->image->isValid()) {
+            if (Storage::exists($post->image))
+                Storage::delete($post->image);
+
+            $nameFile = Str::of($request->title)->slug('-').'.'.$request->image->getClientOriginalExtension();
+
+            $image = $request->image->storeAs('posts', $nameFile);
+            $data['image'] = $image;
+        }
+
+        $post->update($data);
 
         return redirect()
                 ->route('posts.index')
