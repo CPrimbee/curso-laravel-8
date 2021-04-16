dcb2e0410f564fa18e968cf02ae4266734bd20d3
CPrimbee <cprimbee@gmail.com>
2021-04-16 13:38:25 -0300

    Scalfolding auth

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
